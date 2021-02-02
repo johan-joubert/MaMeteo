bb63572141a59ea339f54d1c6699ec94dc06c0aa
Johan <njoubert06@gmail.com>
2021-02-02 10:23:46 +0100

    start project Ma Meteo

---
+++ components/Accueil.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, TextInput, Button } from 'react-native'
+
+class Accueil extends React.Component {
+    render() {
+        return (
+            <View>
+                <TextInput placeholder='Ville' />
+                <Button title='ville' onPress={() =>{}}/>
+            </View>
+        )
+    }
+}
